567e7db67eb5cd33a3ac1cdf2cf981d5639f1bbc
Rayan Waked <rayan@waked.dev>
2024-08-08 19:41:36 -0700

    Initial Commit

---
+++ Quisine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quisine
+//
+//  Created by Rayan Waked on 8/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quisine/QuisineApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuisineApp.swift
+//  Quisine
+//
+//  Created by Rayan Waked on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct QuisineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
